295961cb1c5a911469c0b0c124373476ca323a9c
Evan <evancchow@gmail.com>
2014-09-18 16:23:37 -0400

    Added the way to print out items in the hash map. Should finish implementation of the expanding case.

--- hashmap.py
+++ hashmap.py
@@ -12,7 +12,7 @@ class hashmap():
         """ Initialize a hash map with n buckets. """
         self.numBuckets = n
         self.buckets = [None] * (self.numBuckets + 1)
-        self.totalNodeCount = [None] * (self.numBuckets + 1)
+        self.totalNodeCount = [0] * (self.numBuckets + 1)
     
     def put(self, val):
         # Insert at end of linked list for relevant bucket (by hash).
@@ -28,15 +28,14 @@ class hashmap():
         self.totalNodeCount[hashed] += 1
 
         # Rebalance table on average > 3 nodes / bucket, and rehash all keys.
-        if (self.totalNodeCount / self.numBuckets) > 3:
+        if (sum(self.totalNodeCount) / self.numBuckets) > 3:
             self.buckets.extend([None] * (self.numBuckets))
             self.numBuckets *= 2
             for i in range(len(self.numBuckets / 2)):
                 curr = self.buckets[i]
                 while curr:
                     # Move node to new bucket. Corner case: same bucket.
-
-
+                    
                     curr = curr.next
         
     def hash(self, string):
@@ -44,10 +43,30 @@ class hashmap():
         (total sum of all ord(chars)) % (total # of buckets), 
         but we could be fancy and use something like
         "hashlib.md5(string.encode())" . """
-        return (sum((ord(i) for i in string))) % self.numBuckets
+        return (sum((ord(i) for i in str(string)))) % self.numBuckets
 
     def items(self):
         """ Print all items by bucket in the hashmap. """
-        pass
+        for ix, bucket in enumerate(self.buckets):
+            if not isinstance(bucket, Node):
+                print "Bucket {} is empty".format(ix)
+                continue
+            vals = []
+            curr = bucket
+            while curr:
+                vals.append(curr.val)
+                curr = curr.next
+            print "Bucket {0} has: {items!s}".format(ix, items=vals)
+
+    def numNodes(self):
+        """ Return total # of nodes. """
+        return self.totalNumNodes
+
+    def numBuckets(self):
+        """ Return total # of buckets. """
+        return self.numBuckets
 
 hm = hashmap()        
+hm.put(1)
+hm.put(2)
+hm.put(3)
